3dbbbba713ac62e9327e6294e83ec44c63a02934
salesucation <rhildred@salesucation.ca>
2025-02-15 20:06:27 +0000

    add sitemap

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
 from app import app, pages
 import os
 import shutil
+import json
 
 client = app.test_client();
 
-def load(path):
+def load(path, sitemap):
     res = client.get(path)
     if path.endswith("index"):
         path = path.replace("index", "")
@@ -13,11 +14,23 @@ def load(path):
     except:
         pass
     f = open(f"{os.getcwd()}/dist/{path}/index.html", "wb")
+    sitemap["urlset"].append({"url":{"loc": f"/{path}/index.html"}})
     f.write(res.data)  
     f.close()
 
 if os.path.exists(f"{os.getcwd()}/dist"):
     shutil.rmtree(f"{os.getcwd()}/dist")
 shutil.copytree(f"{os.getcwd()}/static", f"{os.getcwd()}/dist/static")
+sitemap = {"urlset":[]};
 for page in pages:
-    load(page.path)
+    load(page.path, sitemap)
+
+fin = open(f"{os.getcwd()}/package.json")
+oPackage = json.load(fin)
+fin.close()
+f = open(f"{os.getcwd()}/dist/sitemap.xml", "w")
+f.write('<?xml version="1.0" encoding="UTF-8"?>\n<urlset>')
+for url in sitemap["urlset"]:
+    f.write(f"<url><loc>{oPackage["homepage"]}{url["url"]["loc"]}</loc></url>");
+f.write("</urlset>")  
+f.close()
