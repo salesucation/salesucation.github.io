e6d57add0ece2d9b2d4c361bf191ee596a25d359
Rich Hildred <rhildred@gmail.com>
2025-05-07 23:35:11 -0400

    rough in structure of English index

--- pages/en/index/LayoutIndex.jsx
+++ pages/en/index/LayoutIndex.jsx
@@ -1,60 +1,58 @@
 import "../../../layouts/LayoutDefault.scss";
-export default function({children}){
-    return <>
-  <header className="container">
-  <nav role="menu">
-			<label data-role="burger"><input type="checkbox" /></label>
-			<ul role="menubar">
-				<li><strong>salesucation</strong></li>
-			</ul>
-			<ul role="menuitem">
-        <li>
-        <form role="search" method="get" action="https://wpico.mwm/">
+import Maintenance from "./Maintenance.mdx";
+import Operations from "./Operations.mdx";
+export default function ({ children }) {
+  return <>
+    <header className="container">
+      <nav role="menu">
+        <label data-role="burger"><input type="checkbox" /></label>
+        <ul role="menubar">
+          <li><strong>salesucation</strong></li>
+        </ul>
+        <ul role="menuitem">
+          <li>
+            <form role="search" method="get" action="https://wpico.mwm/">
               <input name="s" type="search" placeholder="Search" />
               <input type="submit" value="Search" />
-            </form>          
+            </form>
 
-        </li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Contact</a></li>
-			</ul>
-		</nav>
-  </header>
-  <main className="container">
-    <article>
-      <header>
-        <h1>Home</h1>
-      </header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        posuere tortor et ipsum tincidunt, quis hendrerit diam vehicula. Vivamus
-        at dui ac nisi placerat facilisis egestas ut lorem. Nunc in ligula
-        accumsan, faucibus justo egestas, facilisis libero.
-      </p>
-      <p>
-        Morbi eget felis eu tellus scelerisque lobortis. Vestibulum feugiat
-        imperdiet sollicitudin. Vestibulum mattis ante sed fringilla pharetra.
-        Suspendisse a eleifend elit. Aliquam maximus, diam sed egestas
-        vulputate, dui magna malesuada leo, non maximus lorem dolor sed nibh.
-      </p>
-      <footer></footer>
-    </article>
-    <article>
-    {children}
-    </article>
-  </main>
-  <footer className="container">
-    <nav>
-      <ul>
-        <li>
-          <a href="https://gfip.mwm/legal/">Legal</a>
-        </li>
-        <li>
-          <a href="https://gfip.mwm/newsletter/">Newsletter</a>
-        </li>
-      </ul>
-    </nav>
-  </footer>
-</>
-  
+          </li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main className="container">
+      <div className="hero">
+        {children}
+      </div>
+      <h2>Why Salesucation?</h2>
+      <div className="grid">
+        <article>
+          <Operations />
+        </article>
+        <article>
+          <Maintenance />
+        </article>
+      </div>
+      <h2>Our special sauce</h2>
+      <div class="grid">
+        <article>
+          <Operations />
+        </article>
+        <article>
+          <Operations />
+        </article>
+        <article>
+          <Maintenance />
+        </article>
+      </div>
+      <h2>Articles</h2>
+      <figure></figure>
+    </main>
+    <footer className="container">
+      &copy; Salesucation.com Inc. 2012-{new Date().getFullYear()}
+    </footer>
+  </>
+
 }
